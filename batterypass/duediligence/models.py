c7faae7f6574bf8a082fb1f2a29327d21aa5435b
tridianthor <tridianto.putra@live.com>
2025-02-13 17:53:38 +0700

    improved upload

--- batterypass/duediligence/models.py
+++ batterypass/duediligence/models.py
@@ -5,6 +5,6 @@ import uuid
 # Create your models here.
 class SupplyChainDueDiligence(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    supply_chain_due_diligence_report = models.FileField()
-    third_party_aussurances = models.FileField(null=True, blank=True)
-    supply_chain_indicies = models.FloatField(null=True, blank=True)
+    supply_chain_due_diligence_report = models.TextField(null=False)
+    third_party_assurances = models.TextField(null=False)
+    supply_chain_indices = models.FloatField(null=False, default=0)
