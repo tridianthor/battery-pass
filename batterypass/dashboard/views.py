4f7e5eb6d5900b159c9b2331b7cc1a37ed81a370
tridianthor <tridianto.putra@live.com>
2025-03-17 16:40:07 +0700

    added more data in detail, fixed chart layouts

--- batterypass/dashboard/views.py
+++ batterypass/dashboard/views.py
@@ -113,7 +113,7 @@ def dashboard(request, pk=None, code=None):
 def batteries(request):
     return render(request, "battery-list.html")
 
-def summary(request, pk=None, code=None):
+def get_battery(pk=None, code=None, chart_width = 300, chart_width_wide=350, chart_height = 300):
     if pk is None and code is None:
         raise Exception("No primary key or code provided")
     
@@ -152,22 +152,39 @@ def summary(request, pk=None, code=None):
         recycled_content_lead_values.append(item.pre_consumer_share)
         recycled_content_lead_values.append(item.post_consumer_share)
     
-    chart_width = 300
-    chart_height = 300
-    
     carbon_footprint_fig = px.pie(carbon_footprint_df, values=carbon_footprint_values, names=carbon_footprint_names, title='Carbon Footprint per Lifecycle Stage')
-    carbon_footprint_fig.update_layout(height=chart_height)
+    carbon_footprint_fig.update_layout(width=chart_width_wide, height=chart_height)
+    carbon_footprint_fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
     
     recycled_content_cobalt_fig = px.pie(values=recycled_content_cobalt_values, names=recycled_content_names, title='Recycled Content - Cobalt')
-
+    recycled_content_cobalt_fig.update_layout(width=chart_width, height=chart_height)
+    recycled_content_cobalt_fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
     recycled_content_lithium_fig = px.pie(values=recycled_content_lithium_values, names=recycled_content_names, title='Recycled Content - Lithium')
-
+    recycled_content_lithium_fig.update_layout(width=chart_width, height=chart_height)
+    recycled_content_lithium_fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
     recycled_content_nickel_fig = px.pie(values=recycled_content_nickel_values, names=recycled_content_names, title='Recycled Content - Nickel')
-
+    recycled_content_nickel_fig.update_layout(width=chart_width, height=chart_height)
+    recycled_content_nickel_fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
     recycled_content_lead_fig = px.pie(values=recycled_content_lead_values, names=recycled_content_names, title='Recycled Content - Lead')
-
+    recycled_content_lead_fig.update_layout(width=chart_width, height=chart_height)
+    recycled_content_lead_fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
     
-    context = {
+    return {
         'product': product,
         'carbon_footprint_fig': carbon_footprint_fig.to_html(),
         'recycled_content_cobalt_fig': recycled_content_cobalt_fig.to_html(),
@@ -175,70 +192,13 @@ def summary(request, pk=None, code=None):
         'recycled_content_nickel_fig': recycled_content_nickel_fig.to_html(),
         'recycled_content_lead_fig': recycled_content_lead_fig.to_html()
     }
+
+def summary(request, pk=None, code=None):
+    context = get_battery(pk=pk, chart_width=250, chart_width_wide=550, chart_height=300)
     
     return render(request, "battery-summary.html", context)
 
 def detail(request, pk=None, code=None):
-    if pk is None and code is None:
-        raise Exception("No primary key or code provided")
-    
-    if pk is not None:
-        product = get_object_or_404(GeneralProductInformation, pk=pk)
-    else:
-        product = get_object_or_404(GeneralProductInformation, battery_id=code)
-    
-    carbon_footprint_df = product.carbon_footprint.carbon_footprint_per_lifecycle_stage.all()
-    recycled_content_cobalt_df = product.circularity.recycled_content.filter(recycled_material='Cobalt')
-    recycled_content_lithium_df = product.circularity.recycled_content.filter(recycled_material='Lithium')
-    recycled_content_nickel_df = product.circularity.recycled_content.filter(recycled_material='Nickel')
-    recycled_content_lead_df = product.circularity.recycled_content.filter(recycled_material='Lead')
-    
-    carbon_footprint_names = list(item['lifecycle_stage'] for item in carbon_footprint_df.values())
-    carbon_footprint_values = list(item['carbon_footprint'] for item in carbon_footprint_df.values())
-    
-    recycled_content_names = ["Pre consumer share", "Post consumer share"]
-    recycled_content_cobalt_values = []
-    for item in recycled_content_cobalt_df:
-        recycled_content_cobalt_values.append(item.pre_consumer_share)
-        recycled_content_cobalt_values.append(item.post_consumer_share)
-    
-    recycled_content_lithium_values = []
-    for item in recycled_content_lithium_df:
-        recycled_content_lithium_values.append(item.pre_consumer_share)
-        recycled_content_lithium_values.append(item.post_consumer_share)
-    
-    recycled_content_nickel_values = []
-    for item in recycled_content_nickel_df:
-        recycled_content_nickel_values.append(item.pre_consumer_share)
-        recycled_content_nickel_values.append(item.post_consumer_share)
-    
-    recycled_content_lead_values = []
-    for item in recycled_content_lead_df:
-        recycled_content_lead_values.append(item.pre_consumer_share)
-        recycled_content_lead_values.append(item.post_consumer_share)
-    
-    chart_width = 300
-    chart_height = 300
-    
-    carbon_footprint_fig = px.pie(carbon_footprint_df, values=carbon_footprint_values, names=carbon_footprint_names, title='Carbon Footprint per Lifecycle Stage')
-    carbon_footprint_fig.update_layout(height=chart_height)
-    
-    recycled_content_cobalt_fig = px.pie(values=recycled_content_cobalt_values, names=recycled_content_names, title='Recycled Content - Cobalt')
-
-    recycled_content_lithium_fig = px.pie(values=recycled_content_lithium_values, names=recycled_content_names, title='Recycled Content - Lithium')
-
-    recycled_content_nickel_fig = px.pie(values=recycled_content_nickel_values, names=recycled_content_names, title='Recycled Content - Nickel')
-
-    recycled_content_lead_fig = px.pie(values=recycled_content_lead_values, names=recycled_content_names, title='Recycled Content - Lead')
-
-    
-    context = {
-        'product': product,
-        'carbon_footprint_fig': carbon_footprint_fig.to_html(),
-        'recycled_content_cobalt_fig': recycled_content_cobalt_fig.to_html(),
-        'recycled_content_lithium_fig': recycled_content_lithium_fig.to_html(),
-        'recycled_content_nickel_fig': recycled_content_nickel_fig.to_html(),
-        'recycled_content_lead_fig': recycled_content_lead_fig.to_html()
-    }
+    context = get_battery(pk, chart_width=400, chart_width_wide=350, chart_height=300)
     
     return render(request, "battery-detail.html", context)
