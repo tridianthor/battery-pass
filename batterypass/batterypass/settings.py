551be394978004d93e2b1750ceb8db6c4219581f
tridianthor <tridianto.putra@live.com>
2025-03-19 17:22:20 +0700

    added qr code scanner progress

--- batterypass/batterypass/settings.py
+++ batterypass/batterypass/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'accounts.Account'
 
-SITE_URL = "http://192.168.1.125:8000" #dev
+#SITE_URL = "http://192.168.1.125:8000" #dev
+SITE_URL = "http://localhost:8000" #dev
 
 # Application definition
 
